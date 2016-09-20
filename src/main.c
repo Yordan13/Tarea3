df71f339f41458758b491a02eb87cbd4ea6a9c63
Yordan13 <jjimenez1937@gmail.com>
2016-09-19 20:52:11 -0600

    Todos los datos de la version 0.2.1

--- src/main.c
+++ src/main.c
@@ -2,13 +2,13 @@
 
 int main(int argc, char[] argv){
 	printf("Esto es una prueba de versionamiento en git\n");
-	printf("En estado actual la version 0.1.1\n");
+	printf("En estado actual la version 0.2.1\n");
 	printf("Todo el contenido-------------......\n");
 	printf("-----------------------------------\n");
 	printf("-----------------------------------\n");
 	printf("-----------------------------------\n");
 	printf("-----------------------------------\n");
-	printf("-----------------------------------\n");
+	printf("Aqui se cambio el codigo");
 	printf("-----------------------------------\n");
 	printf("-----------------------------------\n");
 	printf("-----------------------------------\n");
