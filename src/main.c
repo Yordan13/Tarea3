e60195a3e3f7fa09a230c85d8d2c8f4332fb3dac
Yordan13 <jjimenez1937@gmail.com>
2016-09-24 19:25:12 -0600

    nuevos cambios

--- src/main.c
+++ src/main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, char[] argv){
-	printf("Esto es una prueba de versionamiento en git\n");
-	printf("En estado actual la version 0.2.1\n");
+	printf("Este es el branch MASTER------------\n");
 	printf("Todo el contenido-------------......\n");
 	printf("-----------------------------------\n");
 	printf("-----------------------------------\n");
 	printf("-----------------------------------\n");
 	printf("-----------------------------------\n");
-	printf("Aqui se cambio el codigo");
 	printf("-----------------------------------\n");
 	printf("-----------------------------------\n");
 	printf("-----------------------------------\n");
